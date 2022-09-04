a60e7284180da3ec2dbc70fec4ee8011a975e5f3
Arthur Budkovskiy <artik531@gmail.com>
2022-09-05 00:54:14 +0300

    deleted not improtant

--- hm1/src/utils.js
+++
@@ -1,8 +0,0 @@
-export const formatError = text => `
-<span style="color: red;">
-${text}
-</span>`
-
-export function errorHide() {
-    document.getElementById("errorMessage").style.display = "none";
-}
